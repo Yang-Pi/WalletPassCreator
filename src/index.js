25db8d432042fba55a88f4ed1f0fd6007f8c2500
yaroslav.pylaev <56zeBVPax8kPgjM>
2022-04-19 19:34:57 +0300

    Fix problems with certificate info

--- src/index.js
+++ src/index.js
@@ -3,42 +3,44 @@ const { Template } = require("@walletpass/pass-js");
 const fs = require('fs');
 
 async function main() {
-    const template = await buildPassTemplate();
-    const pass = await configurePass(template);
-    await writePassToFile(pass, 'test-cinema.pkpass');
+  const template = await buildPassTemplate();
+  const pass = await configurePass(template);
+  await writePassToFile(pass, 'test-cinema.pkpass');
 }
 
 async function buildPassTemplate() {
   const template = await Template.load(
       './pass-template.pass');
-  template.teamIdentifier = process.env.WALLET_TEAM_IDENTIFIER;
   
   await template.loadCertificate(
-      "./certificates/pass.com.pass-builder.pem",
-      process.env.WALLET_PEM_PRIVATE_KEY_PASSPHRASE
-  );
+      "./certificates/com.example.passbook.pem",
+      process.env.WALLET_PEM_PRIVATE_KEY_PASSPHRASE);
+
+  template.passTypeIdentifier = process.env.PASS_ID;
+  template.teamIdentifier = process.env.TEAM_ID;
 
   return template;
 }
 
 async function configurePass(template) {
   const pass = template.createPass();
+  
   pass.primaryFields.add({ 
-    key: "event", 
-    label: "Film", 
-    value: 'TEST' 
-  });
+      key: "event", 
+      label: "Film", 
+      value: 'TEST' });
+      
   pass.barcodes = [{
-    message: "1234567890",  
-    format: 'PKBarcodeFormatQR',
-    messageEncoding: 'iso-8859-1'
-  }];
+      message: "1234567890",  
+      format: 'PKBarcodeFormatQR',
+      messageEncoding: 'iso-8859-1'}];
 
   return pass;
 }
 
 async function writePassToFile(pass, filename) {
-  await fs.writeFile(filename, await pass.asBuffer(), () => {})
+  console.log(JSON.stringify(pass));
+  fs.writeFileSync(filename, await pass.asBuffer())
 }
 
 main();
